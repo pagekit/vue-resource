0bad808d4e201ae392def8b5e5fe628919b2fee6
Steffan <steffan@schiewe.biz>
2015-07-01 19:35:18 +0200

    update build script

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -1,7 +1,35 @@
-module.exports = {
-    entry: "./src/index",
-    output: {
-        path: "./dist",
-        filename: "vue-resource.js"
+var webpack = require("webpack");
+var version = require("../package.json").version;
+var banner =
+    "/**\n" +
+    " * vue-resource v" + version + "\n" +
+    " * https://github.com/vuejs/vue-resource\n" +
+    " * Released under the MIT License.\n" +
+    " */\n";
+
+module.exports = [
+
+    {
+        entry: "./src/index",
+        output: {
+            path: "./dist",
+            filename: "vue-resource.js"
+        },
+        plugins: [
+            new webpack.BannerPlugin(banner, {raw: true})
+        ]
+    },
+
+    {
+        entry: "./src/index",
+        output: {
+            path: "./dist",
+            filename: "vue-resource.min.js"
+        },
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin,
+            new webpack.BannerPlugin(banner, {raw: true})
+        ]
     }
-};
+
+];
