3d8012e86c8b65ef18e74a6dfb52e9e1142e32d0
Malte <malte@yootheme.com>
2016-01-13 17:28:03 +0100

    refactor client

--- src/http/client/default.js
+++
@@ -1,9 +0,0 @@
-/**
- * Default client.
- */
-
-var xhrClient = require('./xhr');
-
-module.exports = function (request) {
-    return (request.client || xhrClient)(request);
-};
--- src/http/header.js
+++ src/http/header.js
@@ -21,6 +21,57 @@ module.exports = {
         }
 
         return request;
+    },
+
+    response: function (response) {
+
+        if (response.headers) {
+
+            var headers = parseHeaders(response.headers);
+
+            response.headers = function (name) {
+
+                if (name) {
+                    return headers[_.toLower(name)];
+                }
+
+                return headers;
+            }
+
+        }
+
+        return response;
+
     }
 
 };
+
+function parseHeaders(str) {
+
+    var headers = {}, value, name, i;
+
+    if (_.isString(str)) {
+        _.each(str.split('\n'), function (row) {
+
+            i = row.indexOf(':');
+            name = _.trim(_.toLower(row.slice(0, i)));
+            value = _.trim(row.slice(i + 1));
+
+            if (headers[name]) {
+
+                if (_.isArray(headers[name])) {
+                    headers[name].push(value);
+                } else {
+                    headers[name] = [headers[name], value];
+                }
+
+            } else {
+
+                headers[name] = value;
+            }
+
+        });
+    }
+
+    return headers;
+}
