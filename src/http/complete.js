4c9273a32db67de0d2ae0b3202213724ef46cb2d
Ivan Kulikov <ikoolik@gmail.com>
2016-05-27 18:18:31 +0300

    add onComplete listener

---
+++ src/http/complete.js
@@ -0,0 +1,18 @@
+/**
+ * Before Interceptor.
+ */
+
+var _ = require('../util');
+
+module.exports = {
+
+    response: function (response) {
+
+        if (_.isFunction(response.request.onComplete)) {
+            response.request.onComplete.call(this, response);
+        }
+
+        return response;
+    }
+
+};
