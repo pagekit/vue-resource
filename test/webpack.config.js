adc7f440c76719efe3b9604a05834cae75a85574
Steffan <steffan@schiewe.biz>
2017-01-28 15:22:40 +0100

    update dependencies update build scripts

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    entry: __dirname + "/index.js",
+    entry: __dirname + '/index.js',
     output: {
-        path: __dirname + "/",
-        filename: "specs.js"
+        path: __dirname + '/',
+        filename: 'specs.js'
     },
     module: {
         loaders: [
-            {test: /.js/, exclude: /node_modules/, loader: 'babel', query: {presets: [['es2015', {modules: false}]]}}
+            {test: /\.js$/, loader: 'buble-loader', exclude: /node_modules/}
         ]
     }
 };
