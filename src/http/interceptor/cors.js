3901f82cb3e39f8661c97675328102dc40bb1459
Steffan <steffan@schiewe.biz>
2017-01-31 21:31:51 +0100

    fix Vue SSR issues #453

--- src/http/interceptor/cors.js
+++ src/http/interceptor/cors.js
@@ -4,32 +4,37 @@
 
 import Url from '../../url/index';
 import xdrClient from '../client/xdr';
-import { isBoolean } from '../../util';
+import { inBrowser } from '../../util';
 
-const ORIGIN_URL = Url.parse(location.href);
-const SUPPORTS_CORS = 'withCredentials' in new XMLHttpRequest();
+const SUPPORTS_CORS = inBrowser && 'withCredentials' in new XMLHttpRequest();
 
 export default function (request, next) {
 
-    if (!isBoolean(request.crossOrigin) && crossOrigin(request)) {
-        request.crossOrigin = true;
-    }
+    Object.defineProperty(request, 'crossOrigin', {
 
-    if (request.crossOrigin) {
+        get() {
 
-        if (!SUPPORTS_CORS) {
-            request.client = xdrClient;
+            if (inBrowser) {
+
+                var orgUrl = Url.parse(location.href);
+                var reqUrl = Url.parse(request.getUrl());
+
+                if (reqUrl.protocol !== orgUrl.protocol || reqUrl.host !== orgUrl.host) {
+
+                    if (!SUPPORTS_CORS) {
+                        request.client = xdrClient;
+                    }
+
+                    delete request.emulateHTTP;
+
+                    return true;
+                }
+            }
+
+            return false;
         }
 
-        delete request.emulateHTTP;
-    }
+    });
 
     next();
-}
-
-function crossOrigin(request) {
-
-    var requestUrl = Url.parse(Url(request));
-
-    return (requestUrl.protocol !== ORIGIN_URL.protocol || requestUrl.host !== ORIGIN_URL.host);
 }
