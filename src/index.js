f6c3cde2f6d4f2ac3dad1845091256ffff55a103
janschoenherr <jan@yootheme.com>
2021-03-02 11:52:03 +0100

    Add additional check if plugin has already been registered

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function plugin(Vue) {
     });
 }
 
-if (typeof window !== 'undefined' && window.Vue) {
+if (typeof window !== 'undefined' && window.Vue && !window.Vue.resource) {
     window.Vue.use(plugin);
 }
 
